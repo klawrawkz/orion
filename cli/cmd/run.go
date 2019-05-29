2502b4d8bb9a06701c86f4084f7c71ee895b431a
Ian Philpot <ian.philpot@microsoft.com>
2019-05-29 16:18:58 -0400

    Init thoughts on run cmd

--- cli/cmd/run.go
+++ cli/cmd/run.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -15,15 +13,49 @@ var runCmd = &cobra.Command{
 	Short: "Run local test harness",
 	Long:  `Run local test harness`,
 	Run: func(cmd *cobra.Command, args []string) {
+		remoteLocation := ""
 		if docker {
-			fmt.Println("orion run --docker")
+			remoteLocation = getLocation()
 		} else {
-			fmt.Println("orion run")
+			remoteLocation = getLocationWithoutDocker()
 		}
+		localLocation, err := downloadScript(remoteLocation)
+		if err != nil {
+			panic(err)
+		}
+		runScript(localLocation)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(runCmd)
 	runCmd.Flags().BoolVarP(&docker, "docker", "d", true, "Run test harness in local docker")
+}
+
+func downloadInitSh() {
+
+}
+
+func downloadDockerfile() {
+
+}
+
+func downloadMageFile() {
+
+}
+
+func getLocation() string {
+	return "https://raw.githubusercontent.com/microsoft/cobalt/master/test-harness/local-run.sh"
+}
+
+func getLocationWithoutDocker() string {
+	return "https://raw.githubusercontent.com/microsoft/cobalt/master/test-harness/local-run-wo-docker.sh"
+}
+
+func downloadScript(scriptFile string) (string, error) {
+	return "pwd", nil
+}
+
+func runScript(script string) {
+	// Run script file
 }
